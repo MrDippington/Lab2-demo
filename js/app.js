eac55b8895e173edbf517e3cf85feb6ed84743ec
ryan sanders <ryanpaul.smg@gmail.com>
2019-05-06 20:35:24 -0700

    initial file setup

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var likesGamesOfThrones = prompt('Do you like Game of Thrones?');
+
+likesGamesOfThrones = likesGamesOfThrones.toLowerCase();
+
+if(likesGamesOfThrones === 'yes'){
+  alert('me too!');
+} else {
+  alert('how can you not like Game Of Thrones?');
+}
+
+var likesMozzarellaSticks = prompt('Do you love Mozzarella Sticks?');
+
+likesMozzarellaSticks = likesMozzarellaSticks.toLowerCase();
+
+if(likesMozzarellaSticks === 'yes'){
+  alert('Awesome, they\'re my favorite!');
+} else {
+  alert('You\'re crazy!');
+}
+

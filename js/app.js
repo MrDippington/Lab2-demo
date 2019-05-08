2096bd6cd15d9e7648375fc943551064b8946f97
ryan sanders <ryanpaul.smg@gmail.com>
2019-05-07 23:59:36 -0700

    final questions and touches

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-'use strict'
+
+
 var userName = prompt('Greetings, classmate. What\'s your name?');
 console.log();
 
-alert('Thank you for telling me you name, ' + userName + '!');
+alert('Thank you, ' + userName + '! Welcome to my page!');
+
+
 
 var firstName = prompt('Do you remember my first name?');
 console.log(firstName + 'Ryan');
@@ -11,21 +14,53 @@ firstName = firstName.toLowerCase();
 if(firstName === 'ryan'){
   alert('Nice! I am honored that you remember my name!');
 } else {
-  alert('That\'s ok. I\'m not good with names either. My first name is Ryan.');
+  alert('Incorrect, ' + userName + ', but that\'s ok. I\'m not good with names either. My first name is Ryan.');
 }
 
-var work = prompt(userName + ', do you remember where I currently work?')
+
+
+var work = prompt(userName + ', do I currently work at Fedex or UPS?')
 console.log(work + 'FedEx');
 
 work = work.toLowerCase();
 if(work === 'fedex'){
   alert('Man, you have a great memory!');
 } else {
-  alert('It\'s cool, ' + userName + '. I work at Fedex.');
+  alert('Nope, but it\'s cool, ' + userName + '. I work at Fedex.');
 }
 
 
 
+var from = prompt('For the next few questions, please answer Yes or No. Am I from Seattle, Washington, ' + userName + '?');
+console.log();
+
+from = from.toLowerCase();
+if(from === 'no'){
+  alert('That\'s right! I\'m from Augusta, Georgia, ' + userName + '!');
+} else {
+  alert('WRONG, ' + userName + '! I\'m from Augusta, Georgia.');
+}
 
 
 
+var military = prompt('Did I serve in the military, ' + userName + '?')
+console.log();
+
+military = military.toLowerCase();
+if(military === 'no'){
+  alert('Now that was an easy one, ' + userName + "! I did.");
+} else {
+  alert('Correct, ' + userName + '!');
+}
+
+
+
+var service = prompt('What branch did I serve in, ' + userName + '? Army, or Navy?')
+console.log();
+
+service = service.toLowerCase();
+if(service === 'army'){
+  alert('Right you are, ' + userName + '!');
+} else {
+  alert('Oh no, ' + userName + '. I was in the army.');
+}

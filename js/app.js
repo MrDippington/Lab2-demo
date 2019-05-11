5605d8350070359f55f5c6b2e09f67614d33f925
Martin Balke <Mrtnbalke@gmail.com>
2019-05-11 13:58:48 -0700

    We've added functions and structure.

--- js/app.js
+++ js/app.js
@@ -2,67 +2,76 @@
 
 var correctAnswers = 0;
 
-var lastName = prompt('Is my last name Williams?');
-console.log(lastName + ' is Sanders');
 
-lastName = lastName.toLowerCase();
-if(lastName === 'yes' || lastName === 'y'){
-  alert('No, my last name is Sanders.');
-} else {
-  correctAnswers = correctAnswers+1;
-  alert('Correct!');
+function no(answer){
+  if(answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n'){
+    correctAnswers++;
+    alert('Correct!');
+  } else {
+    alert('No, my last name is Sanders.');
+  }
 }
 
+var lastName = prompt('Is my last name Williams?');
+console.log(lastName + ' is Sanders');
+no(lastName);
+
+var from = prompt('Am I from Seattle?');
+console.log(from + ' I am not from Seattle');
+no(from);
+
+function yes(answer){
+  if(answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y'){
+    correctAnswers = correctAnswers++;
+    alert('Got that right!');
+  } else {
+    alert('Wrong. I do work at Fedex.');
+  }
+}
 
 
 var work = prompt('Do I currently work at Fedex?');
 console.log(work + ' is FedEx');
-
-work = work.toLowerCase();
-if(work === 'yes' || work === 'y'){
-  correctAnswers = correctAnswers+1;
-  alert('Got that right!');
-} else {
-  alert('Wrong. I do work at Fedex.');
-}
-
-
-
-var from = prompt('Am I from Seattle?');
-console.log(from + ' I am not from Seattle');
-
-from = from.toLowerCase();
-if(from === 'no' || from === 'n'){
-  correctAnswers = correctAnswers+1;
-  alert('That\'s right! I\'m from Augusta, Georgia!');
-} else {
-  alert('WRONG! I\'m from Augusta, Georgia.');
-}
-
-
+yes(work);
 
 var military = prompt('Did I serve in the military?');
 console.log(military + ' I did serve in the military');
-
-military = military.toLowerCase();
-if(military === 'no' || military === 'n'){
-  alert('I served in the army.');
-} else {
-  correctAnswers = correctAnswers+1;
-  alert('Correct!');
-}
-
+yes(military);
 
 var fan = prompt('Am I a Game of Thrones Fan?');
 console.log(fan + ' I am a Game of Thrones Fan');
+yes(fan);
 
-fan = fan.toLowerCase();
-if(fan === 'yes' || fan === 'y'){
-  correctAnswers = correctAnswers+1;
-  alert('YES! WINTER IS HERE!');
-} else {
-  alert('Incorrect.');
-}
+
+
+// from = from.toLowerCase();
+// if(from === 'no' || from === 'n'){
+//   correctAnswers = correctAnswers++;
+//   alert('That\'s right! I\'m from Augusta, Georgia!');
+// } else {
+//   alert('WRONG! I\'m from Augusta, Georgia.');
+// }
+
+
+
+
+// military = military.toLowerCase();
+// if(military === 'no' || military === 'n'){
+//   alert('I served in the army.');
+// } else {
+//   correctAnswers = correctAnswers++;
+//   alert('Correct!');
+// }
+
+
+
+// fan = fan.toLowerCase();
+// if(fan === 'yes' || fan === 'y'){
+//   correctAnswers = correctAnswers++;
+//   alert('YES! WINTER IS HERE!');
+// } else {
+//   alert('Incorrect.');
+// }
 
 var faveAnswer = '6';
 var fave = prompt('What is my favorite number?');
@@ -70,7 +79,7 @@ console.log(fave + ' My favorite number is 6');
 
 for(var i=1; i<4; i++){
   if(faveAnswer === fave){
-    correctAnswers = correctAnswers+1;
+    correctAnswers = correctAnswers++;
     alert('RIGHT!!!');
     break;
   } else if(fave < faveAnswer) {
@@ -88,7 +97,7 @@ for(var i=1; i<4; i++){
 
 // for(var heroList=1; heroList<6; heroList++){
 //   if(hero === heroList){
-//     correctAnswers = correctAnswers+1;
+//     correctAnswers = correctAnswers++;
 //     alert('YOU KNOW ME SO WELL!!!');
 //     break;
 //   } else {
@@ -106,7 +115,7 @@ var correctAnswers = prompt('You guessed ' + correctAnswers + ' questions right 
 
 
 // while(i < list.length){
-//   correctAnswers = correctAnswers+1;
+//   correctAnswers = correctAnswers++;
 //   list = prompt('Good. Who else?');
 // } else {
 //   list = prompt('Try Again.');

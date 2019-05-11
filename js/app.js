31b3b8acdfe5eeecee4de67d0a1402a1873d4809
ryan sanders <ryanpaul.smg@gmail.com>
2019-05-10 21:22:26 -0700

    extra questions and tally

--- js/app.js
+++ js/app.js
@@ -1,66 +1,149 @@
+'use strict';
 
+var correctAnswers = 0;
 
-var userName = prompt('Greetings, classmate. What\'s your name?');
-console.log();
+var lastName = prompt('Is my last name Williams?');
+console.log(lastName + ' is Sanders');
 
-alert('Thank you, ' + userName + '! Welcome to my page!');
-
-
-
-var firstName = prompt('Do you remember my first name?');
-console.log(firstName + 'Ryan');
-
-firstName = firstName.toLowerCase();
-if(firstName === 'ryan'){
-  alert('Nice! I am honored that you remember my name!');
+lastName = lastName.toLowerCase();
+if(lastName === 'yes' || lastName === 'y'){
+  alert('No, my last name is Sanders.');
 } else {
-  alert('Incorrect, ' + userName + ', but that\'s ok. I\'m not good with names either. My first name is Ryan.');
+  correctAnswers = correctAnswers+1;
+  alert('Correct!');
 }
 
 
 
-var work = prompt(userName + ', do I currently work at Fedex or UPS?')
-console.log(work + 'FedEx');
+var work = prompt('Do I currently work at Fedex?');
+console.log(work + ' is FedEx');
 
 work = work.toLowerCase();
-if(work === 'fedex'){
-  alert('Man, you have a great memory!');
+if(work === 'yes' || work === 'y'){
+  correctAnswers = correctAnswers+1;
+  alert('Got that right!');
 } else {
-  alert('Nope, but it\'s cool, ' + userName + '. I work at Fedex.');
+  alert('Wrong. I do work at Fedex.');
 }
 
 
 
-var from = prompt('For the next few questions, please answer Yes or No. Am I from Seattle, Washington, ' + userName + '?');
-console.log();
+var from = prompt('Am I from Seattle?');
+console.log(from + ' I am not from Seattle');
 
 from = from.toLowerCase();
-if(from === 'no'){
-  alert('That\'s right! I\'m from Augusta, Georgia, ' + userName + '!');
+if(from === 'no' || from === 'n'){
+  correctAnswers = correctAnswers+1;
+  alert('That\'s right! I\'m from Augusta, Georgia!');
 } else {
-  alert('WRONG, ' + userName + '! I\'m from Augusta, Georgia.');
+  alert('WRONG! I\'m from Augusta, Georgia.');
 }
 
 
 
-var military = prompt('Did I serve in the military, ' + userName + '?')
-console.log();
+var military = prompt('Did I serve in the military?');
+console.log(military + ' I did serve in the military');
 
 military = military.toLowerCase();
-if(military === 'no'){
-  alert('Now that was an easy one, ' + userName + "! I did.");
+if(military === 'no' || military === 'n'){
+  alert('I served in the army.');
 } else {
-  alert('Correct, ' + userName + '!');
+  correctAnswers = correctAnswers+1;
+  alert('Correct!');
 }
 
 
+var fan = prompt('Am I a Game of Thrones Fan?');
+console.log(fan + ' I am a Game of Thrones Fan');
 
-var service = prompt('What branch did I serve in, ' + userName + '? Army, or Navy?')
-console.log();
-
-service = service.toLowerCase();
-if(service === 'army'){
-  alert('Right you are, ' + userName + '!');
+fan = fan.toLowerCase();
+if(fan === 'yes' || fan === 'y'){
+  correctAnswers = correctAnswers+1;
+  alert('YES! WINTER IS HERE!');
 } else {
-  alert('Oh no, ' + userName + '. I was in the army.');
+  alert('Incorrect.');
 }
+
+var faveAnswer = '6';
+var fave = prompt('What is my favorite number?');
+console.log(fave + ' My favorite number is 6');
+
+for(var i=1; i<4; i++){
+  if(faveAnswer === fave){
+    correctAnswers = correctAnswers+1;
+    alert('RIGHT!!!');
+    break;
+  } else if(fave < faveAnswer) {
+    fave = prompt('Guess higher.');
+  } else if(fave > faveAnswer){
+    fave = prompt('Guess Lower');
+  }
+}
+
+// var heroList = ['Black Panther', 'Iron Man', 'Batman', 'Spiderman', 'Wolverine', 'The Hulk'];
+// var hero = prompt('Can you name one of my favorite super-heroes?');
+// console.log(heroList + ' are my favorite super heroes');
+
+// heroList = heroList.toLowerCase();
+
+// for(var heroList=1; heroList<6; heroList++){
+//   if(hero === heroList){
+//     correctAnswers = correctAnswers+1;
+//     alert('YOU KNOW ME SO WELL!!!');
+//     break;
+//   } else {
+//     hero === prompt('Guess again, loser.');
+//   }
+// }
+
+var correctAnswers = prompt('You guessed ' + correctAnswers + ' questions right out of 6.');
+
+// var heroList = ['Black Panther', 'Iron Man', 'Captain America', 'Spiderman', 'Doctor Strange', 'The Hulk'];
+// var Avengers = prompt('Name some of my favorite Avengers heroes.');
+// console.log('Favorite Avengers' + list);
+
+// list = list.toLowerCase();
+
+
+// while(i < list.length){
+//   correctAnswers = correctAnswers+1;
+//   list = prompt('Good. Who else?');
+// } else {
+//   list = prompt('Try Again.');
+
+// }
+
+
+// var correctAnswers = prompt('You guessed ' + correctAnswers + ' questions right out of 6.');
+
+
+
+// var heroList = ['Black Panther', 'Iron Man', 'Captain America', 'Spiderman', 'Doctor Strange', 'The Hulk'];
+
+// heroList[0]
+// heroList[1]
+// heroList[2]
+// heroList[3]
+
+
+
+// let heroAnswer = prompt('Fav hero bro?');
+
+// for (var tries = 0; tries < 4; tries++){
+
+//   for (var hero = 0; hero < 6; hero++){
+//     var currentHero = heroList[i]
+
+
+
+//   };
+
+// };
+
+// correct answer given code
+
+// answer matches correct responsw
+
+// incorrect answer
+
+
